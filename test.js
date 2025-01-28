a35ff0c3d525f739c3641fc5f21e967354b6fba8
bahadurCorvit <fawad301bahadur@gmail.com>
2025-01-29 00:26:39 +0500

    Update

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
-const arr = [1, 2, 3];
-console.log(arr.fill(0)); // [0, 
+const now = new Date();
+console.log("Day of the Month:", now.getDate());
+console.log("Day of the Week:", now.getDay());
+console.log("Full Year:", now.getFullYear());
+console.log("Hour:", now.getHours());
+console.log("Minutes:", now.getMinutes());
+console.log("Seconds:", now.getSeconds());
+console.log("Locale Date:", now.toLocaleDateString());
+console.log("UTC Date:", now.toUTCString());
